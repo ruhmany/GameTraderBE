755d2c1057e5dc099473e7b1da52e8133c2bd2d0
abdelrahman.elnaggar <abdelrahman.elnaggar@sewedy.com.eg>
2025-08-09 01:38:16 +0300

    Update Edit User DTO

--- GameTrader/GameTrader.Core/DTOs/UserDTOs/EditUserDTO.cs
+++ GameTrader/GameTrader.Core/DTOs/UserDTOs/EditUserDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameTrader.Core.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,6 @@ namespace GameTrader.Core.DTOs.UserDTOs
     public class EditUserDTO : AddUserDTO
     {
         public string UserId { get; set; }
+        public RoleEnum Role { get; set; }
     }
 }
